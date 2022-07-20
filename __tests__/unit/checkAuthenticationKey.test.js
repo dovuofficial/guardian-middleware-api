844934fa1b1bbe9477650bf27d8197358a0cc97c
Justyn Spooner <justyn@dovu.io>
2022-07-20 11:11:50 +0100

    Run prettier on test files

--- __tests__/unit/checkAuthenticationKey.test.js
+++ __tests__/unit/checkAuthenticationKey.test.js
@@ -1,18 +1,20 @@
-import Config from "app/config"
-import Validation from "app/validators"
+import Config from 'app/config'
+import Validation from 'app/validators'
 
-test("Ensure that the authentication key is valid", () => {
-	const isValidKey = Validation.checkAuthenticationKey(Config.authenticationKey)
+test('Ensure that the authentication key is valid', () => {
+	const isValidKey = Validation.checkAuthenticationKey(
+		Config.authenticationKey
+	)
 
 	expect(isValidKey).toBe(true)
 })
 
-test("Ensure that a bad authentication key is invalid", () => {
-	const isValidKey = Validation.checkAuthenticationKey("1234")
+test('Ensure that a bad authentication key is invalid', () => {
+	const isValidKey = Validation.checkAuthenticationKey('1234')
 
 	expect(isValidKey).toBe(false)
 })
 
-test("Ensure that a authentication key is at least 10 characters long", () => {
+test('Ensure that a authentication key is at least 10 characters long', () => {
 	expect(Config.authenticationKeyValid()).toBe(true)
 })
--- __tests__/unit/middleware/onlyGet.test.js
+++ __tests__/unit/middleware/onlyGet.test.js
@@ -1,11 +1,11 @@
 // App imports
-import Config from "app/config"
-import Language from "app/constants/language"
-import onlyGet from "app/middleware/onlyGet"
+import Config from 'app/config'
+import Language from 'app/constants/language'
+import onlyGet from 'app/middleware/onlyGet'
 
 // Mocks
-import MockedApiResponse from "mocks/apiResponse"
-import MockedApiRequest from "mocks/apiRequest"
+import MockedApiResponse from 'mocks/apiResponse'
+import MockedApiRequest from 'mocks/apiRequest'
 
 const { onlyPostResponse } = Language.middleware
 
@@ -13,16 +13,22 @@ const mockedHandler = () => 'ok'
 const handlerWithMiddleware = onlyGet(mockedHandler)
 const mockedApiResponse = MockedApiResponse.mock()
 
-test("Expect that a request with a GET method succeeds", async () => {
+test('Expect that a request with a GET method succeeds', async () => {
 	const mockedApiRequest = MockedApiRequest.mock()
-	const response = await handlerWithMiddleware(mockedApiRequest, mockedApiResponse)
+	const response = await handlerWithMiddleware(
+		mockedApiRequest,
+		mockedApiResponse
+	)
 
-	expect(response).toBe("ok")
+	expect(response).toBe('ok')
 })
 
-test("Expect that a request with a POST method fails", async () => {
-	const mockedApiRequest = MockedApiRequest.mock({}, "POST")
-	const response = await handlerWithMiddleware(mockedApiRequest, mockedApiResponse)
+test('Expect that a request with a POST method fails', async () => {
+	const mockedApiRequest = MockedApiRequest.mock({}, 'POST')
+	const response = await handlerWithMiddleware(
+		mockedApiRequest,
+		mockedApiResponse
+	)
 
-	expect(response.reason).toBe(onlyPostResponse.notAllowed("POST"))
+	expect(response.reason).toBe(onlyPostResponse.notAllowed('POST'))
 })
