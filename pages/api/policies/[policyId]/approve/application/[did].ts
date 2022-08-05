470fd61de7feb504a5ab385aec14ba772537e5a7
Matt Smithies <flyinggazelle@hotmail.co.uk>
2022-08-05 15:00:09 +0100

    ensure role for approving application

--- pages/api/policies/[policyId]/approve/application/[did].ts
+++ pages/api/policies/[policyId]/approve/application/[did].ts
@@ -4,12 +4,13 @@ import useGuardianContext from 'src/context/useGuardianContext'
 import approveApplicationHandler from 'src/handler/policies/approveApplicationHandler'
 import withAuthentication from 'src/middleware/withAuthentication'
 import withHmac from 'src/middleware/withHmac'
-import ensureStandardRegistryOwner from 'src/middleware/ensureStandardRegistryOwner'
+import ensureRole from 'src/middleware/ensureRole'
+import { Role } from 'src/config'
 
 export default prepare(
 	onlyPut,
 	withHmac,
 	useGuardianContext,
 	withAuthentication,
-	ensureStandardRegistryOwner
+	ensureRole(Role.STANDARD_REGISTRY)
 )(approveApplicationHandler)
