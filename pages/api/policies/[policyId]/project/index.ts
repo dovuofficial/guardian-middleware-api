8943994242d3ba827b97a30cd380f0109ed76592
Matt Smithies <flyinggazelle@hotmail.co.uk>
2022-08-05 15:02:45 +0100

    Route and handler for submitting a EP

---
+++ pages/api/policies/[policyId]/project/index.ts
@@ -0,0 +1,16 @@
+import onlyPost from 'src/middleware/onlyPost'
+import prepare from 'src/utils/prepare'
+import useGuardianContext from 'src/context/useGuardianContext'
+import ecologicalProjectHandler from 'src/handler/policies/ecologicalProjectHandler'
+import withAuthentication from 'src/middleware/withAuthentication'
+import withHmac from 'src/middleware/withHmac'
+import ensureRole from 'src/middleware/ensureRole'
+import { Role } from 'src/config'
+
+export default prepare(
+	onlyPost,
+	withHmac,
+	useGuardianContext,
+	withAuthentication,
+	ensureRole(Role.REGISTRANT)
+)(ecologicalProjectHandler)
