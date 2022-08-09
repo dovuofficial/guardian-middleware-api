b809c60524a50fc21656d31ad0c284bb00098170
Justyn Spooner <justyn@dovu.io>
2022-08-09 15:18:48 +0100

    Add Role check to submitting a new project

--- pages/api/policies/[policyId]/register/index.ts
+++ pages/api/policies/[policyId]/register/index.ts
@@ -4,10 +4,13 @@ import useGuardianContext from 'src/context/useGuardianContext'
 import registerProjectHandler from 'src/handler/policies/registerProjectHandler'
 import withAuthentication from 'src/middleware/withAuthentication'
 import withHmac from 'src/middleware/withHmac'
+import ensureRole from 'src/middleware/ensureRole'
+import { Role } from 'src/config'
 
 export default prepare(
 	onlyPost,
 	withHmac,
 	useGuardianContext,
-	withAuthentication
+	withAuthentication,
+	ensureRole(Role.REGISTRANT)
 )(registerProjectHandler)
