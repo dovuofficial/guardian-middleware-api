70598cfd835d263f725e9254ba460913bdfa3a93
Justyn Spooner <justyn@dovu.io>
2022-08-09 15:18:31 +0100

    Fix lint

--- src/middleware/ensureRole.ts
+++ src/middleware/ensureRole.ts
@@ -4,52 +4,51 @@ import Language from 'src/constants/language'
 import Response from 'src/response'
 import { Role } from 'src/config'
 
-const ensureRole = (role: Role) => {
-	return (handler: NextApiHandler) => {
-		return async (req: GuardianMiddlewareRequest, res: NextApiResponse) => {
-			const { accessToken } = req
-			const { guardian } = req.context
-			const { policyId } = req.query
+const ensureRole =
+	(role: Role) =>
+	(handler: NextApiHandler) =>
+	async (req: GuardianMiddlewareRequest, res: NextApiResponse) => {
+		const { accessToken } = req
+		const { guardian } = req.context
+		const { policyId } = req.query
 
-			const { ensureRole, withAuthenticationResponse } =
-				Language.middleware
+		const { ensureRole: ensureRoleMessage, withAuthenticationResponse } =
+			Language.middleware
 
-			if (!policyId) {
-				return Response.unprocessibleEntity(
-					res,
-					ensureRole.policyRequired
-				)
-			}
-
-			if (!accessToken) {
-				return Response.unprocessibleEntity(
-					res,
-					withAuthenticationResponse.noAccessToken
-				)
-			}
-
-			const policies = await guardian.policies.list(accessToken)
-			const policy = policies.find((policy) => policy.id === policyId)
-
-			if (!policy) {
-				return Response.unprocessibleEntity(
-					res,
-					ensureRole.policyDoesNotExist
-				)
-			}
-
-			// @ts-ignore (TODO: Can fix later.)
-			const hasRoleInPolicy = !!policy.userRoles.find(
-				(userRole) => userRole.toLowerCase() === role.toLowerCase()
+		if (!policyId) {
+			return Response.unprocessibleEntity(
+				res,
+				ensureRoleMessage.policyRequired
 			)
-
-			if (hasRoleInPolicy) {
-				return handler(req, res)
-			}
-
-			return Response.unauthorised(res, ensureRole[role])
 		}
+
+		if (!accessToken) {
+			return Response.unprocessibleEntity(
+				res,
+				withAuthenticationResponse.noAccessToken
+			)
+		}
+
+		const policies = await guardian.policies.list(accessToken)
+		const policy = policies.find((p) => p.id === policyId)
+
+		if (!policy) {
+			return Response.unprocessibleEntity(
+				res,
+				ensureRoleMessage.policyDoesNotExist
+			)
+		}
+
+		// @ts-ignore TODO: Policy needs typing
+		const hasRoleInPolicy = !!policy.userRoles.find(
+			(userRole) => userRole.toLowerCase() === role.toLowerCase()
+		)
+
+		if (hasRoleInPolicy) {
+			return handler(req, res)
+		}
+
+		return Response.unauthorised(res, ensureRoleMessage[role])
 	}
-}
 
 export default ensureRole
