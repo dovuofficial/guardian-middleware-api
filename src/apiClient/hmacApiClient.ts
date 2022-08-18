1721bc13a044bf57db4f63c4064c23483ac6d48d
Justyn Spooner <justyn@dovu.io>
2022-08-18 16:06:08 +0100

    Adds full test suite for the Cool Farm Tool policy flow

--- src/apiClient/hmacApiClient.ts
+++ src/apiClient/hmacApiClient.ts
@@ -1,13 +1,16 @@
-import axios from 'axios'
+import axios, { AxiosInstance } from 'axios'
 import { generateHeaders } from 'src/utils/hmac'
 import env from 'src/config'
 
-let hmacInstance
+let hmacInstance: AxiosInstance | undefined
 
-const getHmacInstance = () => {
+const SECONDS = 1000
+
+const getHmacInstance = (): AxiosInstance => {
 	if (!hmacInstance) {
 		hmacInstance = axios.create({
 			baseURL: env.apiUrl,
+			timeout: 60 * SECONDS,
 		})
 		hmacInstance.interceptors.request.use((config) => {
 			// Generate HMAC headers for every request
