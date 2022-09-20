1d56790b55d11bd90f2e6d6fade4e5638211f9e9
Justyn Spooner <justyn@dovu.io>
2022-09-20 11:14:20 +0100

    Rework how we conditionally open up the trustchain middleware

--- pages/api/policies/[policyId]/trustchains/index.ts
+++ pages/api/policies/[policyId]/trustchains/index.ts
@@ -5,18 +5,22 @@ import withAuthentication from 'src/middleware/withAuthentication'
 import withHmac from 'src/middleware/withHmac'
 import ensureRole from 'src/middleware/ensureRole'
 import { Role } from 'src/config/guardianTags'
-import exceptionFilter from 'src/middleware/exceptionFilter'
 import onlyGet from 'src/middleware/onlyGet'
 import config from 'src/config'
 
-const isProtected = !config.publicTrustChainAccess
-const emptyHandler = (handler) => (req, res) => handler(req, res)
+const generateMiddleware = () => {
+	const base = [onlyGet, useGuardianContext]
 
-export default prepare(
-	exceptionFilter,
-	onlyGet,
-	useGuardianContext,
-	isProtected ? withHmac : emptyHandler,
-	isProtected ? withAuthentication : emptyHandler,
-	isProtected ? ensureRole(Role.STANDARD_REGISTRY) : emptyHandler
-)(trustChainsHandler)
+	if (!config.publicTrustChainAccess) {
+		return [
+			...base,
+			withHmac,
+			withAuthentication,
+			ensureRole(Role.STANDARD_REGISTRY),
+		]
+	}
+
+	return base
+}
+
+export default prepare(...generateMiddleware())(trustChainsHandler)
