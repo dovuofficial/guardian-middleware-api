721d836be94c182ee7be0cc365aa5664f723108d
Matt Smithies <flyinggazelle@hotmail.co.uk>
2023-12-06 10:20:34 +0000

    update in tags with reference to query routes and state queries

--- src/config/guardianTags.ts
+++ src/config/guardianTags.ts
@@ -7,25 +7,32 @@
 
 export enum Tag {
 	chooseRole = 'choose_role',
-	initialApplicationSubmission = 'create_application',
+	initialProjectSubmission = 'create_ecological_project',
 
 	// Approve an application
-	approveApplicationBlocks = 'registrants_grid',
-	approveApplicationBtn = 'approve_registrant_btn',
+	supplierGrid = 'supplier_grid',
+	// supplierGrid = 'save_copy_ecological_project',
+	approveProjectBtn = 'approve_supplier_btn',
 
-	// Submit document to make an ecological project
-	createEcologicalProject = 'create_farm_form',
+	// Submit document to make site
+	createSite = 'create_site_form',
+
+	sitesForClaim = 'sites_grid',
 
 	// Submit an MRV for an ecological project as an issue request
-	mrvSubmission = 'create_issue_request_form',
+	mrvSubmission = 'create_claim_request_form',
+
+	// Submit document to make a claim
+	createClaim = 'create_claim_request_form',
 
 	// Approve an ecological project
-	approveEcologicalProject = 'approve_farms_grid',
-	approveEcologicalProjectBtn = 'approve_farm_button',
+	approveSite = 'approve_sites_grid',
+	approveSiteBtn = 'approve_site_button',
 
 	// Approve an MRV request
-	approveMrvRequest = 'issue_requests_grid(evident)',
-	approveMrvRequestBtn = 'approve_issue_requests_btn',
+	// approveMrvRequest = 'claim_requests_grid(evident)',
+	approveClaimRequest = 'claim_requests_grid(verifier)',
+	approveClaimRequestBtn = 'approve_claim_requests_btn',
 
 	// Weirdness needed for all document approval
 	approveBtn = 'Option_0',
@@ -40,18 +47,51 @@ export enum Tag {
 
 	// Path to mint token
 	verifierWorkflow = 'verifier_workflow',
-	approveIssueRequestsPage = 'approve_issue_requests_page',
-	mintTokenParent = 'mit_token',
+	approveIssueRequestsPage = 'approve_claim_requests_page',
+	mintTokenParent = 'mint_token',
 }
 
 export enum Role {
 	STANDARD_REGISTRY = 'ADMINISTRATOR',
 	VERIFIER = 'VERIFIER',
-	REGISTRANT = 'REGISTRANT',
+	SUPPLIER = 'SUPPLIER',
 }
 
 export enum MRV {
 	AGRECALC = 'agrecalc',
 	COOL_FARM_TOOL = 'cool-farm-tool',
 	GENERAL_SUPPLY_DOCUMENTATION = 'general-supply-documentation',
+}
+
+export enum EntityState {
+	APPROVED = 'Approved',
+	WAITING = 'Waiting for approval',
+}
+
+/**
+ * This enables the (API) client to query the entity at a particular state of the workflow,
+ * guardian expects different users to query to submit documents.
+ */
+export enum QueryRoute {
+	PROJECTS = 'projects', // registry only
+	APPROVE_SITE = 'approve-site', // registry only
+	CREATE_SITE = 'create-site', // supplier only
+	CREATE_CLAIM = 'create-claim', // supplier only
+	APPROVE_CLAIM = 'approve-claim', // verifier only
+}
+
+export const QueryBlockTag = {
+	[QueryRoute.PROJECTS]: Tag.supplierGrid,
+	[QueryRoute.CREATE_SITE]: Tag.createSite,
+	[QueryRoute.CREATE_CLAIM]: Tag.sitesForClaim,
+	[QueryRoute.APPROVE_SITE]: Tag.approveSite,
+	[QueryRoute.APPROVE_CLAIM]: Tag.approveClaimRequest,
+}
+
+export const QueryRole = {
+	[QueryRoute.PROJECTS]: Role.STANDARD_REGISTRY,
+	[QueryRoute.CREATE_SITE]: Role.SUPPLIER,
+	[QueryRoute.CREATE_CLAIM]: Role.SUPPLIER,
+	[QueryRoute.APPROVE_SITE]: Role.STANDARD_REGISTRY,
+	[QueryRoute.APPROVE_CLAIM]: Role.VERIFIER,
 }
